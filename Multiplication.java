8b3f80f95276f2525e501de4efdef8d4a5d2ea79
KenzaHalil <halilkenza2005@gmail.com>
2024-06-01 22:33:39 +0200

    classes

--- Multiplication.java
+++ Multiplication.java
@@ -3,12 +3,12 @@ class Multiplication extends Operation {
         super(operande1, operande2);
     }
 
-    @Override
+    
     public int valeur() {
         return getOperande1().valeur() * getOperande2().valeur();
     }
 
-    @Override
+    
     public String toString() {
         return "(" + getOperande1() + " * " + getOperande2() + ")";
     }
