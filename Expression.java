dc30902960970612e3d6cd30beccb72bccb7543a
tassadit-ouzia <tassadit.ouzia04@gmail.com>
2024-06-03 22:53:00 +0200

    code final

--- Expression.java
+++ Expression.java
@@ -1,9 +1,3 @@
 public abstract class Expression {
-<<<<<<< HEAD
-    public abstract int valeur();
-    
-    public abstract String toString();
-=======
-    public abstract double valeur();
+    public abstract int valeur();   
 }
->>>>>>> 93c4f76e36cd1be9e674cda33ab81e132cc3a9d9
